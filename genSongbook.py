7c4b53024fc08526382d33fd6efd3f383f307342
jgvictores <jcgvicto@gmail.com>
2015-03-15 17:36:56 +0100

    good interface, must incorporate refractored original functionality

--- genSongbook.py
+++ genSongbook.py
@@ -1,80 +1,26 @@
 #!/usr/bin/python
 
-import os
+import cmd  #-- Thanks: http://pymotw.com/2/cmd/
 
-f = open('songbook.tex', 'w')
+class GenSongbook(cmd.Cmd):
+    """Main class."""
+    prompt = '>> '
+    intro = 'Welcome to GenSongbook. Type "help" for help.'
+    
+    def do_greet(self, person):
+        if person:
+            print "hi,", person
+        else:
+            print 'hi'
+    
+    def help_greet(self):
+        print '\n'.join([ 'greet [person]',
+                           'Greet the named person',
+                           ])
+    
+    def do_EOF(self, line):
+        return True
 
-s = """% songbook.tex
-%\documentclass[11pt,a4paper]{article}  % article format
-\documentclass[11pt,a4paper,openany]{book}  % book format
-\usepackage[margin=0.7in]{geometry}
-%\usepackage[utf8]{inputenc}   % tildes
-\usepackage{graphics}
-\usepackage[dvips]{graphicx}
-\usepackage{hyperref}
-\usepackage{verbatim}
-
-% next if more than 100 chapters.
-\usepackage{titletoc}
-\\titlecontents{chapter}[2.5em]
-{\\addvspace{0.5pc}\\bfseries}
-{\contentslabel{2em}}
-{}
-{\\titlerule*[0.3pc]{.}\contentspage}
-
-\hypersetup{
-    pdftitle={Songbook (English) - Summer 2014},
-    pdfsubject={Songbook (English) - Summer 2014},
-    pdfauthor={jgvictores},
-    pdfkeywords={songbook} {english} {summer} {2014},
-    colorlinks,
-    citecolor=black,
-    filecolor=black,
-    linkcolor=black,
-    urlcolor=black,
-    bookmarks
-}
-
-\makeatletter
-\\renewcommand{\@makechapterhead}[1]{%
-{\setlength{\parindent}{0pt} \\raggedright \\normalfont
-\\bfseries S-\\thechapter.\ #1
-\par\\nobreak\\vspace{10 pt}}}
-\makeatother
-
-\\begin{document}
-\Large
-
-\\title{Songbook (English)}
-\\author{by -j} 
-\date{Summer 2014}
-
-\maketitle
-\cleardoublepage
-\\tableofcontents
-\\newpage  % book format
-
-%-- To force blank page: 
-%\\newpage
-%\\thispagestyle{empty}
-%\\mbox{}
-"""
-
-for dirname, dirnames, filenames in os.walk('/opt/Dropbox/lyrics/english'):
-    for filename in sorted(filenames):
-        s += "%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%\n"
-        name, extension = os.path.splitext(filename)
-        s += "\chapter{" + name + "}\n"
-        s += "\\begin{verbatim}\n"
-        song = open( os.path.join(dirname, filename) )
-        s += song.read()
-        s += "\\end{verbatim}\n"
-        s += "\n"
-
-s += """
-\end{document}
-"""
-
-f.write(s)
-
+if __name__ == '__main__':
+    GenSongbook().cmdloop()
 
