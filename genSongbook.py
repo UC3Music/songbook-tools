3ab9cf49f699abe75336fbdf77aecb5053b43bc0
jgvictores <jcgvicto@gmail.com>
2015-03-15 17:58:37 +0100

    better approach

--- genSongbook.py
+++ genSongbook.py
@@ -1,26 +1,18 @@
 #!/usr/bin/python
 
-import cmd  #-- Thanks: http://pymotw.com/2/cmd/
+import sys
 
-class GenSongbook(cmd.Cmd):
-    """Main class."""
-    prompt = '>> '
-    intro = 'Welcome to GenSongbook. Type "help" for help.'
-    
-    def do_greet(self, person):
-        if person:
-            print "hi,", person
-        else:
-            print 'hi'
-    
-    def help_greet(self):
-        print '\n'.join([ 'greet [person]',
-                           'Greet the named person',
-                           ])
-    
-    def do_EOF(self, line):
-        return True
+def query(question, default):
+    sys.stdout.write(question + " [" + default + "] ? ")
+    choice = raw_input()
+    if choice == '':
+        return default
+    return choice
 
 if __name__ == '__main__':
-    GenSongbook().cmdloop()
-
+    print("----------------------")
+    print("Welcome to genSongbook")
+    print("----------------------")
+    path = query("Please specify the path of the input song directory","./english")
+    print("Will use: " + path)
+    
