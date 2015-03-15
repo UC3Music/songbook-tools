71d9289b21fea5e4a9c9a8c55ecda126f427bc0a
jgvictores <jcgvicto@gmail.com>
2015-03-15 20:17:30 +0100

    almost there

--- genSongbook.py
+++ genSongbook.py
@@ -16,7 +16,7 @@ if __name__ == '__main__':
     print("----------------------")
 
     # Query song directory path string
-    songDirectory = query("Please specify the path of the input song directory","./english")
+    songDirectory = query("Please specify the path of the input song directory","/opt/Dropbox/lyrics/english")
     print("Will use song directory: " + songDirectory)
 
     # Query template file path string
@@ -27,9 +27,21 @@ if __name__ == '__main__':
 
     templateFileFd = open(templateFile, 'r')
     s = templateFileFd.read()
-    sys.stdout.write(s)
+    #sys.stdout.write(s)  #-- Screen output for debugging.
 
-    print("----------------------")
+    rep = ""
+    for dirname, dirnames, filenames in os.walk( songDirectory ):
+        for filename in sorted(filenames):
+            rep += "%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%\n"
+            name, extension = os.path.splitext(filename)
+            rep += "\\chapter{" + name + "}\n"  #-- Note that we use \\ instead of \.
+            rep += "\\begin{verbatim}\n"
+            song = open( os.path.join(dirname, filename) )
+            rep += song.read()
+            rep += "\\end{verbatim}\n"
+            rep += "\n"
+
+    s.replace("genSongbook",rep)
 
     outFd = open("out.tex", 'w')
     outFd.write(s)
