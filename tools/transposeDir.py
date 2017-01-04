5925b48d3c0ba53d6f5f2d60640a47b9c6afd60a
jgvictores <jcgvicto@gmail.com>
2017-01-04 14:40:08 +0100

    fix flow on reusing existing dir

--- tools/transposeDir.py
+++ tools/transposeDir.py
@@ -31,13 +31,17 @@ if __name__ == '__main__':
 
     # Query transposed song directory path string
     transposedSongDirectory = query("Please specify the path of the input song directory","/opt/Dropbox/lyrics/transposed_english")
-    print("Will use transposed song directory (output): " + transposedSongDirectory)
 
     if os.path.isdir(transposedSongDirectory):
         yesNo = query('Path "' + transposedSongDirectory + '" already exists, are you sure (confirm with "yes" without quotes)','no')
-        if yesNo is not "yes":
+        if yesNo != "yes":
             print "Ok, bye!"
             quit()
+        else:
+            print("Will use (existing) transposed song directory (output): " + transposedSongDirectory)
+    else:
+        os.makedirs(transposedSongDirectory)
+        print("Will use (newly created) transposed song directory (output): " + transposedSongDirectory)
 
     # Query transposition
     halfTones = int( query("Please specify half tones of transposition","0") )
