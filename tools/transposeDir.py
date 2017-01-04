2ff4e7393f65f0733d043d2c44322f85edb27169
jgvictores <jcgvicto@gmail.com>
2017-01-04 14:22:20 +0100

    transposeDir walk dir

--- tools/transposeDir.py
+++ tools/transposeDir.py
@@ -1,2 +1,49 @@
 #!/usr/bin/python
 
+import sys, os
+
+import mmap  # Thanks Steven @ http://stackoverflow.com/questions/4940032/search-for-string-in-txt-file-python
+
+import subprocess
+
+import readline
+
+readline.set_completer_delims(' \t\n;')
+readline.parse_and_bind("tab: complete")
+readline.parse_and_bind("set match-hidden-files off")
+
+def query(question, default):
+    sys.stdout.write(question + " [" + default + "] ? ")
+    choice = raw_input()
+    if choice == '':
+        return default
+    return choice
+
+if __name__ == '__main__':
+
+    print("-----------------------")
+    print("Welcome to transposeDir")
+    print("-----------------------")
+
+    # Query song directory path string
+    songDirectory = query("Please specify the path of the input song directory","/opt/Dropbox/lyrics/english")
+    print("Will use song directory (input): " + songDirectory)
+
+    # Query transposed song directory path string
+    transposedSongDirectory = query("Please specify the path of the input song directory","/opt/Dropbox/lyrics/transposed_english")
+    print("Will use transposed song directory (output): " + transposedSongDirectory)
+
+    # Query transposition
+    halfTones = int( query("Please specify half tones of transposition","0") )
+    print("Will use half tones of transposition: " + str(halfTones))
+
+    print("----------------------")
+
+    for dirname, dirnames, filenames in os.walk( songDirectory ):
+        for filename in sorted(filenames):
+            name, extension = os.path.splitext(filename)
+            song = open( os.path.join(dirname, filename) )
+            print name
+            #rep += song.read()
+            song.close()
+
