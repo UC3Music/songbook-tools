41423828223e6e77acbba42f6a0b52ff43eeccbb
jgvictores <jcgvicto@gmail.com>
2017-01-08 16:17:50 +0100

    more exceptions

--- tools/transposeDir.py
+++ tools/transposeDir.py
@@ -28,7 +28,7 @@ def query(question, default):
 
 def process( stringToProcess, processed ):
     #print 'String to process "' + stringToProcess + '".'
-    afterSplit = re.split("  |-|!|\.\.\.|\.\.|: |high|open|bass|riff|notes|m6|7sus2", stringToProcess, 1)  # 3rd parameter is maxsplit # Also works with single space, do this to catch faulty txt.
+    afterSplit = re.split("  |-|!|\.\.\.|\.\.|: |\*|high|open|bass|riff|palm mute|notes|m6|madd11/|m7add11/|7sus2", stringToProcess, 1)  # 3rd parameter is maxsplit # Also works with single space, do this to catch faulty txt.
     #print '* Split by delimiters "' + str(afterSplit) + '".'
     if len(afterSplit[0]) != 0:
         chord = Chord(afterSplit[0])
