68f16915a34b4619aff3e2783a6c05ff6835f292
jgvictores <jcgvicto@gmail.com>
2018-04-08 16:02:30 +0200

    improve out

--- extra-tools/pdf-index-to-file.py
+++ extra-tools/pdf-index-to-file.py
@@ -55,10 +55,10 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(formatter_class = MyArgumentDefaultsHelpFormatter)
 
     parser.add_argument('--input',
-                        help='specify the input pdf file',
+                        help='name of the input pdf file',
                         default='songbook.pdf')
     parser.add_argument('--output',
-                        help='specify the output txt file',
+                        help='name of the output txt file',
                         default='manifest.txt')
     parser.add_argument('--yes',
                         help='accept all, skip all queries',
@@ -72,13 +72,13 @@ if __name__ == '__main__':
         skipQueries = True
 
     # Query the input pdf file
-    path_to_pdf = query("Please specify the input pdf file", args.input, skipQueries)
+    path_to_pdf = query("Please specify the name of the input pdf file", args.input, skipQueries)
     if not os.path.isabs(path_to_pdf):
 	path_to_pdf = os.path.abspath('.') + "/" + path_to_pdf
     print("Will extract manifest from file: " + path_to_pdf)
 
     # Query output string
-    manifest = query("Please specify the output txt file", args.output, skipQueries)
+    manifest = query("Please specify the name of the output txt file", args.output, skipQueries)
     print("Will extract manifest to file: " + manifest)
     manifestFd = open(manifest, 'a')
 
