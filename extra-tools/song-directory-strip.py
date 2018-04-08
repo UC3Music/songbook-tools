c870dcacfed876d672fd5476757738d88b902a08
jgvictores <jcgvicto@gmail.com>
2018-04-08 12:23:34 +0200

    mv yes down

--- extra-tools/song-directory-strip.py
+++ extra-tools/song-directory-strip.py
@@ -33,16 +33,16 @@ if __name__ == '__main__':
 
     parser = argparse.ArgumentParser()
 
-    parser.add_argument('--yes',
-                        help='accept all, skip all queries',
-                        nargs='?',
-                        default='absent')  # required, see below
     parser.add_argument('--input',
                         help='specify the path of the default song (input) directory',
                         default='/home/yo/Dropbox/chords/0-GUITAR/english')
     parser.add_argument('--output',
                         help='specify the path of the default song (output) directory',
                         default='tmp')
+    parser.add_argument('--yes',
+                        help='accept all, skip all queries',
+                        nargs='?',
+                        default='absent')  # required, see below
     args = parser.parse_args()
 
     skipQueries = False
